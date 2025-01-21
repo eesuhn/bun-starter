7436eb7b3f6cba3d03184346db613b77b7831545
eesuhn <eason.yihong@gmail.com>
2025-01-22 02:22:06 +0800

    Setting up config for SDK dev

--- eslint.config.js
+++ eslint.config.js
@@ -18,6 +18,7 @@ export default [
     rules: {
       ...typescriptPlugin.configs.recommended.rules,
       'prettier/prettier': 'warn',
+      '@typescript-eslint/no-explicit-any': 'off',
     },
   },
 ];
